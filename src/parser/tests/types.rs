6479891fbf1f5d8ffa7256d715806e397601b613
Steve Loveless <steve.loveless@gmail.com>
2021-09-07 19:58:52 -0700

    Better string literal parsing

--- src/parser/tests/types.rs
+++ src/parser/tests/types.rs
@@ -25,6 +25,74 @@ fn t_integer_literal_test() {
 }
 
 #[test]
+fn t_string_literal_test() {
+    assert!(RbsParser::parse(Rule::t_symbol_literal, "123").is_err());
+
+    // Double-quote strings
+    {
+        parses_to! {
+            parser: RbsParser, input:  r#""""#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 2, [dq_string_content(1, 1)])]
+        };
+
+        parses_to! {
+            parser: RbsParser, input:  r#""stuff""#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 7, [dq_string_content(1, 6)])]
+        };
+
+        parses_to! {
+            parser: RbsParser, input:  r#""This is a sentence.""#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 21, [dq_string_content(1, 20)])]
+        };
+
+        parses_to! {
+            parser: RbsParser, input:  r#""Ce m'est égale""#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 17, [dq_string_content(1, 16)])]
+        };
+
+        parses_to! {
+            parser: RbsParser, input:  r#""私は亀が好きです""#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 26, [dq_string_content(1, 25)])]
+        };
+
+        // TODO: quote escapes
+        // parses_to! {
+        //     parser: RbsParser, input:  r#""And then I said, \"whoa!\"""#, rule: Rule::t_string_literal,
+        //     tokens: [t_string_literal(0, 27, [dq_string_content(1, 26)])]
+        // };
+    }
+
+    // Single-qoute strings
+    {
+        parses_to! {
+            parser: RbsParser, input:  r#"''"#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 2, [sq_string_content(1, 1)])]
+        };
+
+        parses_to! {
+            parser: RbsParser, input:  r#"'stuff'"#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 7, [sq_string_content(1, 6)])]
+        };
+
+        parses_to! {
+            parser: RbsParser, input:  r#"'This is a sentence.'"#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 21, [sq_string_content(1, 20)])]
+        };
+
+        // TODO: quote escapes
+        // parses_to! {
+        //     parser: RbsParser, input:  r#"'Ce m\'est égale'"#, rule: Rule::t_string_literal,
+        //     tokens: [t_string_literal(0, 17, [sq_string_content(1, 16)])]
+        // };
+
+        parses_to! {
+            parser: RbsParser, input:  r#"'私は亀が好きです'"#, rule: Rule::t_string_literal,
+            tokens: [t_string_literal(0, 26, [sq_string_content(1, 25)])]
+        };
+    }
+}
+
+#[test]
 fn t_symbol_literal_test() {
     assert!(RbsParser::parse(Rule::t_symbol_literal, "foo").is_err());
 
@@ -34,32 +102,32 @@ fn t_symbol_literal_test() {
 
         parses_to! {
             parser: RbsParser, input:  ":@_", rule: Rule::t_symbol_literal,
-            tokens: [t_symbol_literal(0, 3)]
+            tokens: [t_symbol_literal(0, 3, [instance_variable(1, 3)])]
         };
 
         parses_to! {
             parser: RbsParser, input:  ":@a", rule: Rule::t_symbol_literal,
-            tokens: [t_symbol_literal(0, 3)]
+            tokens: [t_symbol_literal(0, 3, [instance_variable(1, 3)])]
         };
 
         parses_to! {
             parser: RbsParser, input:  ":@A", rule: Rule::t_symbol_literal,
-            tokens: [t_symbol_literal(0, 3)]
+            tokens: [t_symbol_literal(0, 3, [instance_variable(1, 3)])]
         };
 
         parses_to! {
             parser: RbsParser, input:  ":@a1", rule: Rule::t_symbol_literal,
-            tokens: [t_symbol_literal(0, 3)]
+            tokens: [t_symbol_literal(0, 3, [instance_variable(1, 3)])]
         };
 
         parses_to! {
             parser: RbsParser, input:  ":@a1", rule: Rule::t_symbol_literal,
-            tokens: [t_symbol_literal(0, 3)]
+            tokens: [t_symbol_literal(0, 3, [instance_variable(1, 3)])]
         };
 
         parses_to! {
             parser: RbsParser, input:  ":@_1", rule: Rule::t_symbol_literal,
-            tokens: [t_symbol_literal(0, 3)]
+            tokens: [t_symbol_literal(0, 3, [instance_variable(1, 3)])]
         };
     }
 
@@ -88,8 +156,8 @@ fn t_symbol_literal_test() {
     // Regular symbol
     {
         parses_to! {
-            parser: RbsParser, input:  ":thing", rule: Rule::t_symbol_literal,
-            tokens: [t_symbol_literal(0, 3)]
+            parser: RbsParser, input: ":thing", rule: Rule::t_symbol_literal,
+            tokens: [t_symbol_literal(0, 6, [method_name(1, 6)])]
         };
     }
 }
